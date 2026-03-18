72da33d7051233f9ae0dd424a989582fe17f44cc
bigcohen2000-stack <bigcohen2000@gmail.com>
2026-03-18 15:39:15 +0200

    Initial Astro site and CMS setup

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+﻿import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import pagefind from "astro-pagefind";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  site: "https://www.nevermind.co.il",
+  publicDir: "./public",
+  output: "static",
+  compressHTML: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
+  image: {
+    domains: ["youtube.com", "i.ytimg.com", "img.youtube.com"],
+  },
+  integrations: [
+    mdx(),
+    react(),
+    sitemap({
+      filter: (page) => page !== "/404",
+      serialize: (item) => ({ ...item, priority: 0.7 }),
+    }),
+    pagefind(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: ["react", "react-dom"],
+          },
+        },
+      },
+    },
+  },
+});
